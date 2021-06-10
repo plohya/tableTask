4443720de689306a58987d97e4b112a4bd3bac45
Siarhei Rachytski <siarhei.rachytski@soft-fx.lv>
2021-06-10 22:52:24 +0300

    V. 1.0.

--- src/js/script.js
+++ src/js/script.js
@@ -1,136 +1,174 @@
 import '../css/style.scss';
 import arr from './arrayOfObjects.js';
 
-class Table{
-  constructor(array) {
-    this.array = array;
-    this.amount = 10; 
-    this.$arrowBtnList = null;
-    this.sortInformation = {
-      title: '',
-      desc: -1
-    };
-    this.$pagBtns = document.getElementById('pagination');
-    this.$table = document.getElementById('tableArea');
-    this.$dynamicList = document.querySelectorAll('.item');
-  }
-    
-  init(){
-    this.drawTitles(this.array);
-    this.$table.innerHTML += `<div id='cellsArea' class='cellsArea'></div>`;
-    this.$cellsArea = document.getElementById('cellsArea');
-    this.drawCells(this.array);
-    this.$pagList();
-    this.$arrowBtnList = document.querySelectorAll('.sortButton');
-    this.$pagPageNum = document.getElementById('list');
-    this.$arbtns = document.querySelectorAll('.sortButton');
-    this.$arbtns.forEach(item => item.addEventListener('click', (e) => this.arrowBtnClick(e.target.parentElement.textContent, e.target)));
-    this.$dynamicList.forEach(item => item.addEventListener('click', (e) => this.dynamicView(e.target)));
-  }
-    
-  drawTitles(){
-    for (let key in this.array[0]) {
-      this.$table.innerHTML += `<div class='tableTopElement'>${key}<span class='sortButton'></span><img class="sortButton" src='img/angle-arrow-down_icon-icons.com_73683.svg'></div>`;
+  class Table{
+    constructor(array) {
+      this.array = array;
+      this.amount = 10; 
+      this.page = 1;
+      this.sortInformation = {
+        title: '',
+        desc: -1
+      };
     }
-  }
-    
-  drawCells(array){
-    this.clearCells();
-    for(let i = 0; i < this.amount; i++) {
-      for(let key in array[i]) {
-        this.$cellsArea.innerHTML += `<div class='tableElement'>${array[i][key]}</div>`;
+
+    // Initializing method
+    init() {
+        this.$table = document.createElement('div');
+        this.$table.classList.add('tableArea');
+        this.$buttonsList = document.createElement('div');
+        this.$buttonsList.classList.add('btnList');
+
+        this.drawDynamicButtons();
+        this.drawPagButtons();
+        this.drawTitles();
+        this.drawCells(this.page);
+    }
+
+    // Method draws Buttons that can be used for dynamic change (amount elements on page) + addEventListeners for them
+    drawDynamicButtons() {
+        this.$buttonsList.innerHTML += `<div class='dynamic-list'><div class='text'>Elements on page:</div><button class='dynamicListItem active'>10</button><button class='dynamicListItem'>20</button><button class='dynamicListItem'>50</button><button class='dynamicListItem'>ALL</button></div>`
+
+        document.querySelector('body').appendChild(this.$buttonsList);
+        document.querySelectorAll('.dynamicListItem').forEach(item => item.addEventListener('click', (e) => 
+                                                          this.dynamicView(e.target.textContent, e.target)));
+    }
+
+    // Method draws Buttons that can be used for pagination (reDrawing if pagination pages got new amount) + addEventListeners for them
+    drawPagButtons(){
+        if(document.querySelector('.pagBtnsList')){
+            document.querySelector('.pagBtnsList').remove();
+        }
+
+        const pagPageAmount = Math.ceil(this.array.length / this.amount);
+        const $pagBtnList = document.createElement('div');
+        $pagBtnList.classList.add('pagBtnsList');
+        $pagBtnList.innerHTML += `<div class='text'>Pagination buttons:</div>`;
+        for(let i = 0; i < pagPageAmount; i++) {
+            $pagBtnList.innerHTML += `<button class='pagListItem'>${i+1}</button>`;
+        }
+
+        this.$buttonsList.appendChild($pagBtnList);
+        document.querySelectorAll('.pagListItem').forEach(item => item.addEventListener('click', (e) => 
+                                                          this.drawCells(e.target.textContent)));
+    }
+
+    // Method that creates an element on the page and fills it by titles from the array of object + creating arrows for sorting  + addEventListeners for them
+    drawTitles() {
+        let $titles = document.createElement('div');
+        $titles.classList.add('titlesArea');
+
+        const keysArray = this.array.map(item => Object.keys(item));
+        this.titlesArray = [];
+
+        for(let i = 0 ; i < keysArray.length; i++) {
+          keysArray[i].forEach(item => {
+            if(!this.titlesArray.includes(item)) {
+              this.titlesArray.push(item);
+            }
+          });
+        }
+        $titles.innerHTML = this.titlesArray.map(item => `<div class='tableTopElement'>${item}<div class='sortButton'>></div></div>`).join(' ');
+        this.$table.appendChild($titles);
+        document.querySelector('body').appendChild(this.$table);
+        document.querySelectorAll('.sortButton').forEach(item => item.addEventListener('click', (e) => this.arrowBtnClick(e.target.parentElement.firstChild.textContent, e.target)));
+    }
+
+    // Method that creates an element on the page and fills it by data from the array of object
+    drawCells(page) {
+      if(document.querySelectorAll('.pagListItem')) {
+        document.querySelectorAll('.pagListItem').forEach(item => this.checkBtn(item));
+        document.querySelectorAll('.pagListItem')[page-1].classList.add('active');
       }
-    }
-  }
+      this.clearCells();
+      let $cellsArea = document.createElement('div');
+      $cellsArea.classList.add('cellsArea');
 
-  $pagList(){
-    this.$pagPageNum = Math.ceil(this.array.length / this.amount);
-    this.$pagBtns.innerHTML = '';
-    for(let i = 0; i < this.$pagPageNum; i++) {
-      this.$pagBtns.innerHTML += `<button class='list_item'>${i+1}</button>`;
-    }
-    this.$pagBtn = document.querySelectorAll('.list_item');
-    this.$pagBtn[0].classList.add('active');
-    this.$pagBtn.forEach(item => item.addEventListener('click', (e) => this.pagChange(e.target)))
-  }
-      
-  pagChange(page){
-    this.$pagBtn.forEach(item => this.checkBtn(item));
-    let start = (page.textContent-1) * this.amount;
-    let end = start + this.amount;
-    console.log(start, end);
-    let a = this.array.slice(start,end);
-    console.log(a);
-    this.drawCells(a);
-    page.classList.toggle('active');
-  }
+      const start = (page - 1) * this.amount;   
+      const end = start + this.amount;
+      let pageArr = this.array.slice(start, end);
 
-  arrowBtnClick(title, btn) {
-      this.$arbtns.forEach(item => this.checkBtn(item));
+        pageArr.forEach(item => {
+        let $cellsRow = document.createElement('div');
+        for (let i = 0; i < this.titlesArray.length; i++) {
+          if(item[this.titlesArray[i]] === undefined) {
+            $cellsRow.innerHTML += `<div class='tableElement'>~</div>`;
+          } else {
+            $cellsRow.innerHTML += `<div class='tableElement'>${item[this.titlesArray[i]]}</div>`;
+          }
+        }
+        $cellsArea.appendChild($cellsRow);
+      });
+
+      this.$table.appendChild($cellsArea);
+    }
+
+    // Method that understand did u sorted it before? what should event do? (sort/reverseSort)
+    arrowBtnClick(title, btn) {
+      if(document.querySelectorAll('.backSortButton')){
+        document.querySelectorAll('.backSortButton').forEach(item => this.checkBtn(item));
+      }
+
       if(this.sortInformation.desc === -1 && this.sortInformation.title === title) {
-      this.reverseSortTable(this.array, title);
-      this.drawCells(this.array);
-      this.sortInformation.desc = 1;
-      this.sortInformation.title = title;
-      btn.classList.remove('backSortButton');
-      this.$pagList();
-    } else {
-      this.sortTable(this.array, title);
-      this.drawCells(this.array);
-      this.sortInformation.desc = -1;
-      this.sortInformation.title = title;
-      btn.classList.add('backSortButton');
-      this.$pagList();
-    }
-  }
+        this.sortTable(title, 'ascending');
+        this.sortInformation.desc = 1;
+        btn.classList.remove('backSortButton');
+      } else {
+        this.sortTable(title, 'descending');
+        this.sortInformation.desc = -1;
+        btn.classList.add('backSortButton');
+      }
 
-  dynamicView(button){
-    this.$dynamicList.forEach(item => this.checkBtn(item));
-    if(button.textContent === 'ALL') {
-      this.amount = this.array.length;
-      this.drawCells(this.array);
-      this.$pagList();
-    } else {
-      this.amount = parseInt(button.textContent);
-      this.dynamicChange();
-      this.$pagList();
+      this.sortInformation.title = title;
+      this.drawPagButtons();
+      this.drawCells(this.page);
     }
-    button.classList.toggle('active');
-  }
-  
-  checkBtn(item){
-    if(item.classList.contains('active')){
-      item.classList.remove('active');
-    } else if(item.classList.contains('backSortButton')) {
-      item.classList.remove('backSortButton');
-    }
-  }
 
-  dynamicChange() {
-    this.clearCells();
-    for(let i = 0; i < this.amount; i++) {
-      for(let key in this.array[i]) {
-        this.$cellsArea.innerHTML += `<div class='tableElement'>${this.array[i][key]}</div>`;
+    // Method that tells drawCells() required amount of elements on each page
+    dynamicView(elementsOnPage, btn) {
+      if(document.querySelectorAll('.active')) {
+        document.querySelectorAll('.active').forEach(item => this.checkBtn(item));
+      }
+      btn.classList.add('active');
+
+        if(elementsOnPage === 'ALL') {
+            this.amount = this.array.length;
+        } else {
+            this.amount = parseInt(elementsOnPage);
+        }
+
+        this.drawPagButtons();
+        this.drawCells(this.page);  
+    }
+
+    // Method that got possibility to sort table in two-ways
+    sortTable(objectField, way) {
+      if(way === 'ascending') {
+        this.array.sort(function(a,b) {
+          return ((a[objectField] < b[objectField]) ? 1 : ((a[objectField] > b[objectField]) ? -1 : 0));
+        });
+      } else if(way === 'descending'){
+        this.array.sort(function(b,a) {
+          return ((a[objectField] < b[objectField]) ? 1 : ((a[objectField] > b[objectField]) ? -1 : 0));
+        });
+      }
+      this.drawCells(this.page);
+    }
+
+    // Method that checks, is there (at nodelist) pressed items?
+    checkBtn(item){
+      if(item.classList.contains('active')){
+        item.classList.remove('active');
+      } else if(item.classList.contains('backSortButton')) {
+        item.classList.remove('backSortButton');
       }
     }
-  }
 
-  sortTable(array, objectField) {
-    return array.sort(function(a,b) {
-        let x = a[objectField], y = b[objectField];
-        return ((x > y) ? 1 : ((x < y) ? -1 : 0));
-    })
-  }
-
-  reverseSortTable(array, objectField) {    
-    return array.sort(function(a,b) {
-        let x = a[objectField], y = b[objectField];
-        return ((x > y) ? -1 : ((x < y) ? 1 : 0));
-    })
-  }
-  
-    clearCells() {       
-      this.$cellsArea.innerHTML = ''; 
+    // Method that removes cellsArea from DOM. All data (not titles) from objects are in cellsArea 
+    clearCells() {
+        if(document.querySelector('.cellsArea')) {
+            document.querySelector('.cellsArea').remove();
+        }
     }
 
 }
