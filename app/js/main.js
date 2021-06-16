50fec6e6b39421c439b9382c7bd0f6b491672a1d
Siarhei Rachytski <siarhei.rachytski@soft-fx.lv>
2021-06-16 14:31:58 +0300

    loyaout ready. need adaptive

---
+++ app/js/main.js
@@ -0,0 +1,106 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./css/style.scss":
+/*!************************!*\
+  !*** ./css/style.scss ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack:///./css/style.scss?");
+
+/***/ }),
+
+/***/ "./js/arrayOfObjects.js":
+/*!******************************!*\
+  !*** ./js/arrayOfObjects.js ***!
+  \******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst arr = [\r\n      {\r\n        \"color\": \"purple\",\r\n        \"type\": \"minivan\",\r\n        \"registration\": '2014',\r\n        \"capacity\": 7\r\n      },\r\n      {\r\n        \"color\": \"white\",\r\n        \"type\": \"duster\",\r\n        \"registration\": '2010',\r\n        \"capacity\": 5\r\n      },\r\n      {\r\n        \"color\": \"orange\", \r\n        \"type\": \"boombox\",\r\n        \"registration\": '2015',\r\n        \"capacity\": 50\r\n      },\r\n      {\r\n        \"color\": \"white\",\r\n        \"type\": \"cheezee\",\r\n        \"registration\": '2001',\r\n        \"capacity\": 16\r\n      },\r\n      {\r\n        \"color\": \"purple\",\r\n        \"type\": \"minivan\",\r\n        \"registration\": '2014',\r\n        \"capacity\": 7\r\n      },\r\n      {\r\n        \"color\": \"white\",\r\n        \"type\": \"duster\",\r\n        \"registration\": '2010',\r\n        \"capacity\": 5\r\n      },\r\n      {\r\n        \"color\": \"orange\", \r\n        \"type\": \"boombox\",\r\n        \"registration\": '2015',\r\n        \"capacity\": 50\r\n      },\r\n      {\r\n        \"color\": \"white\",\r\n        \"type\": \"cheezee\",\r\n        \"registration\": '2001',\r\n        \"capacity\": 16\r\n      },\r\n      {\r\n        \"color\": \"purple\",\r\n        \"type\": \"minivan\",\r\n        \"registration\": '2014',\r\n        \"capacity\": 7\r\n      },\r\n      {\r\n        \"color\": \"white\",\r\n        \"type\": \"duster\",\r\n        \"registration\": '2010',\r\n        \"capacity\": 5\r\n      },\r\n      {\r\n        \"color\": \"orange\", \r\n        \"type\": \"boombox\",\r\n        \"registration\": '2015',\r\n        \"capacity\": 50\r\n      },\r\n      {\r\n        \"color\": \"white\",\r\n        \"type\": \"cheezee\",\r\n        \"registration\": '2001',\r\n        \"capacity\": 16\r\n      },\r\n      {\r\n        \"color\": \"red\",\r\n        \"type\": \"boombox\",\r\n        \"registration\": '2008',\r\n        \"capacity\": 50\r\n      },\r\n     {\r\n        \"color\": \"purple\",\r\n        \"type\": \"minivan\",\r\n        \"registration\": '2014',\r\n        \"capacity\": 7\r\n      },\r\n      {\r\n        \"color\": \"white\",\r\n        \"type\": \"duster\",\r\n        \"registration\": '2010',\r\n        \"capacity\": 5\r\n      },\r\n      {\r\n        \"color\": \"orange\", \r\n        \"type\": \"boombox\",\r\n        \"registration\": '2015',\r\n        \"capacity\": 50\r\n      },\r\n      {\r\n        \"color\": \"purple\",\r\n        \"type\": \"minivan\",\r\n        \"registration\": '2014',\r\n        \"capacity\": 7\r\n      },\r\n      {\r\n        \"color\": \"white\",\r\n        \"type\": \"duster\",\r\n        \"registration\": '2010',\r\n        \"capacity\": 5\r\n      },\r\n      {\r\n        \"color\": \"orange\", \r\n        \"type\": \"boombox\",\r\n        \"registration\": '2015',\r\n        \"capacity\": 50\r\n      },\r\n      {\r\n        \"color\": \"purple\",\r\n        \"type\": \"minivan\",\r\n        \"registration\": '2014',\r\n        \"capacity\": 7\r\n      },\r\n      {\r\n        \"color\": \"white\",\r\n        \"type\": \"duster\",\r\n        \"registration\": '2010',\r\n        \"capacity\": 5\r\n      },\r\n      {\r\n        \"color\": \"orange\", \r\n        \"type\": \"boombox\",\r\n        \"registration\": '2015',\r\n        \"capacity\": 50\r\n      },\r\n      {\r\n        \"color\": \"purple\",\r\n        \"type\": \"minivan\",\r\n        \"registration\": '2014',\r\n        \"capacity\": 7\r\n      },\r\n      {\r\n        \"color\": \"white\",\r\n        \"type\": \"duster\",\r\n        \"registration\": '2010',\r\n        \"capacity\": 5\r\n      },\r\n      {\r\n        \"color\": \"orange\", \r\n        \"type\": \"boombox\",\r\n        \"registration\": '2015',\r\n        \"capacity\": 50\r\n      },\r\n      {\r\n        \"color\": \"orange\", \r\n        \"type\": \"boombox\",\r\n        \"registration\": '2015',\r\n        \"capacity\": 50\r\n      },\r\n      {\r\n        \"color\": \"orange\", \r\n        \"type\": \"boombox\",\r\n        \"registration\": '2015',\r\n        \"capacity\": 50\r\n      },\r\n      {\r\n        \"color\": \"orange\", \r\n        \"type\": \"boombox\",\r\n        \"registration\": '2015',\r\n        \"capacity\": 50\r\n      },\r\n      {\r\n        \"color\": \"orange\", \r\n        \"type\": \"boombox\",\r\n        \"registration\": '2015',\r\n        \"capacity\": 50\r\n      },\r\n      {\r\n        \"color\": \"white\",\r\n        \"type\": \"cheezee\",\r\n        \"registration\": '2001',\r\n        \"capacity\": 16\r\n      },\r\n      {\r\n        \"color\": \"red\",\r\n        \"type\": \"boombox\",\r\n        \"registration\": '2008',\r\n        \"capacity\": 50\r\n      },\r\n      {\r\n        \"color\": \"purple\",\r\n        \"type\": \"minivan\",\r\n        \"registration\": '2014',\r\n        \"capacity\": 7\r\n      },\r\n      {\r\n        \"color\": \"red\",\r\n        \"type\": \"boombox\",\r\n        \"registration\": '2008',\r\n        \"capacity\": 50\r\n      },\r\n      {\r\n        \"color\": \"purple\",\r\n        \"type\": \"minivan\",\r\n        \"registration\": '2014',\r\n        \"capacity\": 7\r\n      },\r\n      {\r\n        \"color\": \"white\",\r\n        \"type\": \"cheezee\",\r\n        \"registration\": '2001',\r\n        \"capacity\": 16\r\n      },\r\n      {\r\n        \"color\": \"red\",\r\n        \"type\": \"boombox\",\r\n        \"registration\": '2008',\r\n        \"capacity\": 50\r\n      },\r\n     {\r\n        \"color\": \"purple\",\r\n        \"type\": \"minivan\",\r\n        \"registration\": '2014',\r\n        \"capacity\": 7\r\n      },\r\n]\r\n\r\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (arr);\n\n//# sourceURL=webpack:///./js/arrayOfObjects.js?");
+
+/***/ }),
+
+/***/ "./js/script.js":
+/*!**********************!*\
+  !*** ./js/script.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _css_style_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../css/style.scss */ \"./css/style.scss\");\n/* harmony import */ var _arrayOfObjects_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./arrayOfObjects.js */ \"./js/arrayOfObjects.js\");\n\r\n\r\n\r\n  class Table{\r\n    constructor(array) {\r\n      this.array = array;\r\n      this.amount = 10; \r\n      this.page = 1;\r\n      this.sortInformation = {\r\n        title: '',\r\n        desc: -1\r\n      };\r\n    }\r\n\r\n    // Initializing method\r\n    init() {\r\n        this.$table = document.createElement('div');\r\n        this.$table.classList.add('tableArea');\r\n        this.$buttonsList = document.createElement('div');\r\n        this.$buttonsList.classList.add('btnList');\r\n\r\n        this.drawDynamicButtons();\r\n        this.drawPagButtons();\r\n        this.drawTitles();\r\n        this.drawCells(this.page);\r\n    }\r\n\r\n    // Method draws Buttons that can be used for dynamic change (amount elements on page) + addEventListeners for them\r\n    drawDynamicButtons() {\r\n        this.$buttonsList.innerHTML += `<div class='dynamic-list'><div class='text'>Elements on page:</div><button class='dynamicListItem active'>10</button><button class='dynamicListItem'>20</button><button class='dynamicListItem'>50</button><button class='dynamicListItem'>ALL</button></div>`\r\n\r\n        document.querySelector('body').appendChild(this.$buttonsList);\r\n        document.querySelectorAll('.dynamicListItem').forEach(item => item.addEventListener('click', (e) => \r\n                                                          this.dynamicView(e.target.textContent, e.target)));\r\n    }\r\n\r\n    // Method draws Buttons that can be used for pagination (reDrawing if pagination pages got new amount) + addEventListeners for them\r\n    drawPagButtons(){\r\n        if(document.querySelector('.pagBtnsList')){\r\n            document.querySelector('.pagBtnsList').remove();\r\n        }\r\n\r\n        const pagPageAmount = Math.ceil(this.array.length / this.amount);\r\n        const $pagBtnList = document.createElement('div');\r\n        $pagBtnList.classList.add('pagBtnsList');\r\n        $pagBtnList.innerHTML += `<div class='text'>Pagination buttons:</div>`;\r\n        for(let i = 0; i < pagPageAmount; i++) {\r\n            $pagBtnList.innerHTML += `<button class='pagListItem'>${i+1}</button>`;\r\n        }\r\n\r\n        this.$buttonsList.appendChild($pagBtnList);\r\n        document.querySelectorAll('.pagListItem').forEach(item => item.addEventListener('click', (e) => \r\n                                                          this.drawCells(e.target.textContent)));\r\n    }\r\n\r\n    // Method that creates an element on the page and fills it by titles from the array of object + creating arrows for sorting  + addEventListeners for them\r\n    drawTitles() {\r\n        let $titles = document.createElement('div');\r\n        $titles.classList.add('titlesArea');\r\n\r\n        const keysArray = this.array.map(item => Object.keys(item));\r\n        this.titlesArray = [];\r\n\r\n        for(let i = 0 ; i < keysArray.length; i++) {\r\n          keysArray[i].forEach(item => {\r\n            if(!this.titlesArray.includes(item)) {\r\n              this.titlesArray.push(item);\r\n            }\r\n          });\r\n        }\r\n        $titles.innerHTML = this.titlesArray.map(item => `<div class='tableTopElement'>${item}<div class='sortButton'>></div></div>`).join(' ');\r\n        this.$table.appendChild($titles);\r\n        document.querySelector('body').appendChild(this.$table);\r\n        document.querySelectorAll('.sortButton').forEach(item => item.addEventListener('click', (e) => this.arrowBtnClick(e.target.parentElement.firstChild.textContent, e.target)));\r\n    }\r\n\r\n    // Method that creates an element on the page and fills it by data from the array of object\r\n    drawCells(page) {\r\n      if(document.querySelectorAll('.pagListItem')) {\r\n        document.querySelectorAll('.pagListItem').forEach(item => this.checkBtn(item));\r\n        document.querySelectorAll('.pagListItem')[page-1].classList.add('active');\r\n      }\r\n      this.clearCells();\r\n      let $cellsArea = document.createElement('div');\r\n      $cellsArea.classList.add('cellsArea');\r\n\r\n      const start = (page - 1) * this.amount;   \r\n      const end = start + this.amount;\r\n      let pageArr = this.array.slice(start, end);\r\n\r\n        pageArr.forEach(item => {\r\n        let $cellsRow = document.createElement('div');\r\n        for (let i = 0; i < this.titlesArray.length; i++) {\r\n          if(item[this.titlesArray[i]] === undefined) {\r\n            $cellsRow.innerHTML += `<div class='tableElement'>~</div>`;\r\n          } else {\r\n            $cellsRow.innerHTML += `<div class='tableElement'>${item[this.titlesArray[i]]}</div>`;\r\n          }\r\n        }\r\n        $cellsArea.appendChild($cellsRow);\r\n      });\r\n\r\n      this.$table.appendChild($cellsArea);\r\n    }\r\n\r\n    // Method that understand did u sorted it before? what should event do? (sort/reverseSort)\r\n    arrowBtnClick(title, btn) {\r\n      if(document.querySelectorAll('.backSortButton')){\r\n        document.querySelectorAll('.backSortButton').forEach(item => this.checkBtn(item));\r\n      }\r\n\r\n      if(this.sortInformation.desc === -1 && this.sortInformation.title === title) {\r\n        this.sortTable(title, 'ascending');\r\n        this.sortInformation.desc = 1;\r\n        btn.classList.remove('backSortButton');\r\n      } else {\r\n        this.sortTable(title, 'descending');\r\n        this.sortInformation.desc = -1;\r\n        btn.classList.add('backSortButton');\r\n      }\r\n\r\n      this.sortInformation.title = title;\r\n      this.drawPagButtons();\r\n      this.drawCells(this.page);\r\n    }\r\n\r\n    // Method that tells drawCells() required amount of elements on each page\r\n    dynamicView(elementsOnPage, btn) {\r\n      if(document.querySelectorAll('.active')) {\r\n        document.querySelectorAll('.active').forEach(item => this.checkBtn(item));\r\n      }\r\n      btn.classList.add('active');\r\n\r\n        if(elementsOnPage === 'ALL') {\r\n            this.amount = this.array.length;\r\n        } else {\r\n            this.amount = parseInt(elementsOnPage);\r\n        }\r\n\r\n        this.drawPagButtons();\r\n        this.drawCells(this.page);  \r\n    }\r\n\r\n    // Method that got possibility to sort table in two-ways\r\n    sortTable(objectField, way) {\r\n      if(way === 'ascending') {\r\n        this.array.sort(function(a,b) {\r\n          return ((a[objectField] < b[objectField]) ? 1 : ((a[objectField] > b[objectField]) ? -1 : 0));\r\n        });\r\n      } else if(way === 'descending'){\r\n        this.array.sort(function(b,a) {\r\n          return ((a[objectField] < b[objectField]) ? 1 : ((a[objectField] > b[objectField]) ? -1 : 0));\r\n        });\r\n      }\r\n      this.drawCells(this.page);\r\n    }\r\n\r\n    // Method that checks, is there (at nodelist) pressed items?\r\n    checkBtn(item){\r\n      if(item.classList.contains('active')){\r\n        item.classList.remove('active');\r\n      } else if(item.classList.contains('backSortButton')) {\r\n        item.classList.remove('backSortButton');\r\n      }\r\n    }\r\n\r\n    // Method that removes cellsArea from DOM. All data (not titles) from objects are in cellsArea \r\n    clearCells() {\r\n        if(document.querySelector('.cellsArea')) {\r\n            document.querySelector('.cellsArea').remove();\r\n        }\r\n    }\r\n\r\n}\r\n\r\nconst table = new Table(_arrayOfObjects_js__WEBPACK_IMPORTED_MODULE_1__.default);\r\ntable.init();\n\n//# sourceURL=webpack:///./js/script.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./js/script.js");
+/******/ 	
+/******/ })()
+;
